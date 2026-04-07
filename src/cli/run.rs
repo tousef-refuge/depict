5f2d577fba98153b66bbf4746e16ea21c82409f4
tousef-refuge <yoyo.alsh2007@gmail.com>
2026-04-07 21:14:31 +0300

    Added project root finder to run_command()

--- src/cli/run.rs
+++ src/cli/run.rs
@@ -1,6 +1,13 @@
 use clap::Subcommand;
+use std::env;
 
 #[derive(Subcommand)]
 pub enum Command {}
 
-pub fn run_command(cmd: Command) {}
+pub fn run_command(command: Command) {
+    let exe_dir = env::current_exe()
+        .ok()
+        .and_then(|p| p.parent().map(|p| p.to_path_buf()))
+        .expect("Failed to get binary directory");
+    let project_root = exe_dir.join("../..");
+}
