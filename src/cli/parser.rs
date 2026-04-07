2a50563f9f30b0cd64f70ecbd0c45b1662328203
tousef-refuge <yoyo.alsh2007@gmail.com>
2026-04-07 18:54:28 +0300

    Added parser

---
+++ src/cli/parser.rs
@@ -0,0 +1,11 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(author, version, about)]
+pub struct Cli {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand)]
+enum Command {}
--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,8 @@
-fn main() {}
+mod cli;
+
+use clap::Parser;
+use cli::parser::Cli;
+
+fn main() {
+    let cli = Cli::parse();
+}
