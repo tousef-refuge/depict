5ef9514b6dfc40ff211f95462eb6ead68c929615
tousef-refuge <yoyo.alsh2007@gmail.com>
2026-04-07 21:39:57 +0300

    Implemented trim function for images

---
+++ py/commands/trim.py
@@ -0,0 +1,9 @@
+from PIL import Image
+
+def trim(file):
+    img = Image.open(file).convert("RGBA")
+
+    bbox = img.split()[3].getbbox()
+    trimmed = img.crop(bbox)
+    trimmed.save(file)
+    print(f"Cropped: {file}")
