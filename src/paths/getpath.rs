695018276b54a09005833eadf20e1538b0eb3a96
tousef-refuge <yoyo.alsh2007@gmail.com>
2026-04-08 07:01:09 +0300

    Added functions to get py/ and venv/ from temp

---
+++ src/paths/getpath.rs
@@ -0,0 +1,19 @@
+use std::path::PathBuf;
+use crate::get_zip;
+use crate::paths::extract::zip_extract;
+
+pub fn get_py() -> PathBuf {
+    zip_extract("py", get_zip!("py"))
+}
+
+pub fn get_venv() -> PathBuf {
+    let mut venv = zip_extract("venv", get_zip!("venv"));
+    if cfg!(windows) {
+        //ai slop mode
+        venv = venv.join("venv").join("Scripts").join("python.exe");
+    } else {
+        //reddit mode
+        venv = venv.join("venv").join("bin").join("python");
+    }
+    venv
+}
