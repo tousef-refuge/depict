6ecc9f8002fd335b1b6e973ca8603319142d9d11
tousef-refuge <yoyo.alsh2007@gmail.com>
2026-04-08 07:39:08 +0300

    Added debug -zipskip flag

--- src/cli/parser.rs
+++ src/cli/parser.rs
@@ -6,4 +6,18 @@ use crate::cli::run;
 pub struct Cli {
     #[command(subcommand)]
     pub command: run::Command,
+
+    #[cfg(feature = "debug")]
+    #[arg(short, long)]
+    pub zipskip: bool,
+}
+
+#[cfg(feature = "debug")]
+pub fn zipskip(cli: &Cli) -> bool {
+    cli.zipskip
+}
+
+#[cfg(not(feature = "debug"))]
+pub fn zipskip(cli: &Cli) -> bool {
+    false
 }
