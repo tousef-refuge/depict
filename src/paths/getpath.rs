0b9645dba144daf784ee021f3ce07ca73804bdd6
tousef-refuge <yoyo.alsh2007@gmail.com>
2026-04-08 08:05:58 +0300

    Implemented -z flag checks in path getters, and set -z default to true

--- src/paths/getpath.rs
+++ src/paths/getpath.rs
@@ -1,13 +1,30 @@
+use std::env;
 use std::path::PathBuf;
 use crate::get_zip;
 use crate::paths::extract::zip_extract;
 
-pub fn get_py() -> PathBuf {
-    zip_extract("py", get_zip!("py"))
+fn get_project_root() -> PathBuf {
+    let exe_dir = env::current_exe()
+        .ok()
+        .and_then(|p| p.parent().map(|p| p.to_path_buf()))
+        .expect("Failed to get binary directory");
+    exe_dir.join("../..")
 }
 
-pub fn get_venv() -> PathBuf {
-    let mut venv = zip_extract("venv", get_zip!("venv"));
+pub fn get_py(zipskip: bool) -> PathBuf {
+    if zipskip {
+        get_project_root()
+    } else {
+        zip_extract("py", get_zip!("py"))
+    }
+}
+
+pub fn get_venv(zipskip: bool) -> PathBuf {
+    let mut venv = if zipskip {
+        PathBuf::from(get_project_root())
+    } else {
+        zip_extract("venv", get_zip!("venv"))
+    };
     if cfg!(windows) {
         //ai slop mode
         venv = venv.join("venv").join("Scripts").join("python.exe");
