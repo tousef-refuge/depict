2c90781693cc76cd60bb1e877dc6e41231221da8
tousef-refuge <yoyo.alsh2007@gmail.com>
2026-04-11 19:51:08 +0300

    Implemented depict-updater

--- src/update/updater.rs
+++ src/update/updater.rs
@@ -1,12 +1,12 @@
 use std::process::{Command, exit, id};
 
-use crate::paths::exe_dir;
+use crate::paths::release_extract;
 
 pub fn run_updater() {
     let pid = id();
     Command::new("depict-updater.exe")
         .arg(pid.to_string())
-        .arg(exe_dir().to_string_lossy().to_string())
+        .arg(release_extract().to_string_lossy().to_string())
         .spawn()
         .unwrap();
     exit(0);
--- updater/src/main.rs
+++ updater/src/main.rs
@@ -1,3 +1,48 @@
+use std::{env, fs, thread};
+use std::path::PathBuf;
+use std::process::Command;
+use std::time::Duration;
+use sysinfo::System;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let pid: usize = args[1].parse().unwrap();
+
+    let install_dir = env::current_exe()
+        .unwrap()
+        .parent()
+        .unwrap()
+        .to_path_buf();
+    let release_extract = PathBuf::from(&args[2]);
+
+    loop {
+        let mut sys = System::new_all();
+        sys.refresh_all();
+
+        if sys.process(sysinfo::Pid::from(pid)).is_none() {
+            break;
+        }
+        thread::sleep(Duration::from_secs(1));
+    }
+    thread::sleep(Duration::from_secs(5));
+
+    for entry in fs::read_dir(release_extract).expect("Failed to read update dir") {
+        let entry = entry.expect("Bad dir entry");
+        let path = entry.path();
+
+        if path.is_file() {
+            let file_name = path.file_name().unwrap();
+
+            let dst = &install_dir.join(file_name);
+            let backup = &install_dir.join(format!("{}.old", file_name.to_string_lossy()));
+
+            let _ = fs::remove_file(&backup);
+            let _ = fs::rename(&dst, &backup);
+
+            fs::copy(&path, &dst).unwrap();
+        }
+    }
+
+    let _ = Command::new(install_dir.join("mycli.exe"))
+        .spawn();
 }
