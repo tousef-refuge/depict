282fa3eaffdb0a1f5d947fe3fa3c62b9c4369bc4
tousef-refuge <yoyo.alsh2007@gmail.com>
2026-04-13 19:28:03 +0300

    Implemented glob matching for --ignore

--- py/input/filters.py
+++ py/input/filters.py
@@ -1,3 +1,6 @@
+from pathspec import PathSpec
+#ROOT = Path(sys.executable).parent if getattr(sys, "frozen", False) else Path.cwd()
+
 filter_type = None
 filter_list = None
 
@@ -8,15 +11,15 @@ def filter_init(sysargs):
     filters = sysargs.filters
     for _type in filters:
         filter_type = _type
-        filter_list = filters[_type]
+        filter_list = PathSpec.from_lines("gitwildmatch", filters[_type])
         break
 
+# noinspection PyUnresolvedReferences
 def skip_file(path):
     global filter_type
     global filter_list
 
     if filter_type is not None:
-        if path in filter_list:
-            return True
+        return filter_list.match_file(path)
 
     return False
