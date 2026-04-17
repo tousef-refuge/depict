858e4ff9a3521528b2ba400396e83a1ba26798b3
tousef-refuge <yoyo.alsh2007@gmail.com>
2026-04-17 14:44:52 +0300

    Implemented depict restore

--- src/cli/run/backup.rs
+++ src/cli/run/backup.rs
@@ -1,3 +1,55 @@
+use colored::Colorize;
+use std::fs;
+use std::path::Path;
+use walkdir::WalkDir;
+
 use crate::cli::Command;
 
-pub fn backup_command(_command: Command) {}
+pub fn backup_command(command: Command) {
+    match command {
+        Command::Restore { path: name } => {
+            let path = Path::new(&name);
+            if !path.exists() {
+                println!("{}", "Path does not exist".red());
+                return;
+            }
+            dir_walk(restore, path);
+        },
+        _ => unreachable!(),
+    }
+}
+
+//man i dont feel like calling python again lmao
+fn dir_walk<F>(func: F, path: &Path)
+where
+    F: Fn(&Path),
+{
+    for entry in WalkDir::new(&path)
+        .into_iter()
+        .filter_map(|e| e.ok())
+    {
+        let path = entry.path();
+        if path.is_file() {
+            func(path);
+        }
+    }
+}
+
+fn restore(path: &Path) {
+    if path.extension().and_then(|e| e.to_str()) != Some("old") {
+        return
+    }
+
+    let file_name = match path.file_name().and_then(|n| n.to_str()) {
+        Some(n) => n,
+        None => return
+    };
+
+    let restored_name = file_name
+        .strip_suffix(".old")
+        .unwrap_or(file_name);
+    let restored = path.with_file_name(restored_name);
+
+    fs::rename(path, &restored).ok();
+    println!("{}{}", "Restored: ".blue(), restored.display());
+}
