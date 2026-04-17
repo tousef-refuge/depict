4e9ed262aeeb3085ced11171e233d66a556bbec9
tousef-refuge <yoyo.alsh2007@gmail.com>
2026-04-17 15:32:06 +0300

    Restructured main.py for easier command implementation

--- py/main.py
+++ py/main.py
@@ -2,26 +2,27 @@ from .commands import *
 from .input import *
 import colorama
 
+COMMANDS = {
+    "trim" : trim,
+    "flip" : flip,
+    "scale" : scale,
+    "resize" : resize,
+    "alpha" : alpha,
+    "invert" : invert,
+    "grayscale" : grayscale,
+    "backup" : backup
+}
+
 def main():
     colorama.init()
     argv = SysArgs()
-    match argv["name"]:
-        case "trim" | "flip" | "scale" | "resize" | "alpha" | "invert" | "grayscale" | "backup":
-            command = {
-                "trim" : trim,
-                "flip" : flip,
-                "scale" : scale,
-                "resize" : resize,
-                "alpha" : alpha,
-                "invert" : invert,
-                "grayscale" : grayscale,
-                "backup" : backup
-            }[argv["name"]]
 
-            if argv["name"] == "backup":
-                argv.file_args["backup"] = True
-            process_path(command, argv)
+    name = argv["name"]
+    if name not in COMMANDS.keys():
+        name_error = f"Invalid function name ({argv["name"]})"
+        raise NameError(name_error)
 
-        case _:
-            name_error = f"Invalid function name ({argv["name"]})"
-            raise NameError(name_error)
+    command = COMMANDS[name]
+    if name == "backup":
+        argv.file_args["backup"] = True
+    process_path(command, argv)
