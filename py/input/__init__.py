61435d11e932d4202f3054c7e4b43b3ac9e60a39
tousef-refuge <yoyo.alsh2007@gmail.com>
2026-04-20 20:36:12 +0300

    Implemented video processing

--- py/input/__init__.py
+++ py/input/__init__.py
@@ -1,2 +1,2 @@
 from .args import SysArgs
-from .paths import process_path
+from .dirwalk import process_path
--- py/input/process.py
+++ py/input/process.py
@@ -2,6 +2,8 @@ from .filters import skip_file
 from colorama import Fore
 from PIL import Image
 from py import image_output
+import cv2
+import numpy as np
 import os
 
 def process(func, sysargs, path):
@@ -23,7 +25,30 @@ def process(func, sysargs, path):
         _image(func, sysargs, path)
 
 def _video(func, sysargs, path):
-    pass
+    cap = cv2.VideoCapture(path)
+
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+    out = cv2.VideoWriter(path, fourcc, fps, (width, height))
+
+    frame_number = 1
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        img = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+        img = func(sysargs, img, f"{path} (Frame {frame_number})")
+        frame_number += 1
+
+        frame = cv2.cvtColor(
+            np.array(img.convert("RGB")),
+            cv2.COLOR_RGB2BGR
+        )
+        out.write(frame)
 
 def _image(func, sysargs, path):
     img = Image.open(path).convert("RGBA")
