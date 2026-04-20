47a0e1ef09f7a2573a6b07c2a480ac210da5fa68
tousef-refuge <yoyo.alsh2007@gmail.com>
2026-04-20 20:26:37 +0300

    Separated process function into different media types

--- py/input/process.py
+++ py/input/process.py
@@ -2,6 +2,7 @@ from .filters import skip_file
 from colorama import Fore
 from PIL import Image
 from py import image_output
+import os
 
 def process(func, sysargs, path):
     if skip_file(path, func):
@@ -15,6 +16,16 @@ def process(func, sysargs, path):
         backup = p.with_name(p.name + ".old")
         shutil.copy(p, backup)
 
+    ext = os.path.splitext(path)[1][1:]
+    if ext == "mp4":
+        _video(func, sysargs, path)
+    else:
+        _image(func, sysargs, path)
+
+def _video(func, sysargs, path):
+    pass
+
+def _image(func, sysargs, path):
     img = Image.open(path).convert("RGBA")
     result = func(sysargs, img, path)
     if not path.lower().endswith(".png"):
