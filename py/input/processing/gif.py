21a34faf91e61bf070ec89c5b7e34cf38397af03
tousef-refuge <yoyo.alsh2007@gmail.com>
2026-04-21 12:45:27 +0300

    Implemented gif processing

--- py/input/processing/gif.py
+++ py/input/processing/gif.py
@@ -1,2 +1,20 @@
+from PIL import Image, ImageSequence
+
 def process(func, sysargs, path):
-    pass
+    gif = Image.open(path)
+
+    frames = []
+    frame_number = 1
+    for frame in ImageSequence.Iterator(gif):
+        img = frame.convert("RGBA")
+        img = func(sysargs, img, f"{path} (Frame {frame_number})")
+        frame_number += 1
+        frames.append(img.convert("RGB"))
+
+    frames[0].save(
+        path,
+        save_all=True,
+        append_images=frames[1:],
+        loop=0,
+        duration=gif.info.get("duration")
+    )
