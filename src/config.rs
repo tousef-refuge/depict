76bb634c7551e071e94360d9b5bac55432d4943a
tousef-refuge <yoyo.alsh2007@gmail.com>
2026-04-24 14:53:51 +0300

    Implemented config functions

--- src/config.rs
+++ src/config.rs
@@ -1,13 +1,50 @@
 use crate::paths::project_root;
+use serde::{Serialize, Deserialize};
 use std::fs;
 use std::path::PathBuf;
 
-fn config_json() -> PathBuf {
-    project_root().join("config.json")
+#[derive(Serialize, Deserialize)]
+pub struct Config {
+    pub test: bool,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Config {
+            test: false
+        }
+    }
 }
 
 pub fn init() {
-    if !config_json().exists() {
-        let _ = fs::write(config_json(), " ");
+    let path = config_json();
+    if !path.exists() {
+        if let Some(parent) = path.parent() {
+            fs::create_dir_all(parent).unwrap();
+        }
+
+        let default = Config::default();
+        let data = serde_json::to_string(&default).unwrap();
+        fs::write(path, data).unwrap();
     }
+}
+
+pub fn load_config() -> Config {
+    let path = config_json();
+    if !path.exists() {
+        return Config::default();
+    }
+
+    let data = fs::read_to_string(path).unwrap();
+    serde_json::from_str(&data).unwrap()
+}
+
+pub fn save_config(config: &Config) {
+    let path = config_json();
+    let data = serde_json::to_string(config).unwrap();
+    fs::write(path, data).unwrap();
+}
+
+fn config_json() -> PathBuf {
+    project_root().join("config.json")
 }
