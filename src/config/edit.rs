2df456a158a3adf834a225692e087e8ed5e54c48
tousef-refuge <yoyo.alsh2007@gmail.com>
2026-04-24 23:39:43 +0300

    Handled edge cases for depict config set and added prettier output

--- src/config/edit.rs
+++ src/config/edit.rs
@@ -1,10 +1,34 @@
+use colored::Colorize;
 use crate::config::json::{load_config, save_config};
 
 pub fn edit_setting(key: String, val: String) {
     let mut config = load_config();
     match key.as_str() {
-        "test" => { config.test = val.parse().unwrap(); }
-        _ => panic!("Invalid key")
+        "test" => edit(&mut config.test, &val, "test"),
+        _ => println!("{} {}", "Invalid configuration key:".red(), key.red().bold()),
     }
     save_config(config);
+}
+
+fn edit<T>(key: &mut T, val: &str, key_name: &str)
+where
+    T: std::str::FromStr,
+{
+    if let Ok(parsed) = val.parse::<T>() {
+        *key = parsed;
+        println!(
+            "{} {} {} {}",
+            "Changed the setting for".blue(),
+            key_name.blue(),
+            "to:".blue(),
+            val.blue().bold()
+        );
+    } else {
+        println!(
+            "{} {}{} {}",
+            "Invalid value given for".red(),
+            key_name.red(),
+            ":".red(),
+            val.red().bold())
+    }
 }
